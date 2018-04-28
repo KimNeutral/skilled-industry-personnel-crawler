4490e5f8f3003d7938100e19ec1274a07c73da45
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-28 19:59:58 +0900

    Init

---
+++ crawler.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+regex = r"(?<=\/)(\d+(?= 페이지))"
+
+p = re.compile(regex)
+
+def spider():
+    page = 1
+    param = {
+        'al_eopjong_gbcd': '11111',
+        'eopjong_gbcd_list': '11111',
+        'eopjong_gbcd': '1',
+        'gegyumo_cd': '',
+        'eopjong_cd': '11111',
+        'eopche_nm': '',
+        'sido_addr': '',
+        'sigungu_addr': ''
+    }
+
+    url = 'https://work.mma.go.kr/caisBYIS/search/byjjecgeomsaek.do'
+    r = requests.post(url, param);
+    soup = BeautifulSoup(r.text, 'lxml')
+    page += 1
+    max_pages = int(p.search(soup.find(class_='topics').text).group())
+
+    while page <= max_pages:
+        url = 'https://work.mma.go.kr/caisBYIS/search/byjjecgeomsaek.do?pageIndex=' + str(page)
+        r = requests.post(url, param);
+        soup = BeautifulSoup(r.text, 'lxml')
+        page += 1
+        for tr in soup.find(class_='brd_list_n').find('tbody').select('tr'):
+            print(tr.find('th').text)
+
+
+spider()
