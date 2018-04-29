e5cddcc6eb6e524e3582b5460fa8a158effbc62e
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-29 23:24:51 +0900

    add: added parameters to settings.py

---
+++ Crawler/settings.py
@@ -0,0 +1,6 @@
+ITEM_PIPELINES = {'newscrawling.pipelines.MongoDBPipeline': 300, }
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "skilled_industry"
+MONGODB_COLLECTION = "companies"
