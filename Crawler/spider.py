fabbab5110e9938abf039e01aa8625f99b9f2716
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-29 21:59:18 +0900

    edit: edited non-necessary variable

--- Crawler/spider.py
+++ Crawler/spider.py
@@ -12,7 +12,7 @@ class CompanySpider(scrapy.Spider):
         for info in response.css('.table_col_type1').css('tbody').css('tr'):
             key = info.css('th::text').extract_first().strip()
             if key == "홈페이지":
-                dic[key] = link = info.css('td a.link_site::attr("href")').extract_first()
+                dic[key] = info.css('td a.link_site::attr("href")').extract_first()
             else:
                 dic[key] = info.css('td::text').extract_first().strip()
         yield dic
