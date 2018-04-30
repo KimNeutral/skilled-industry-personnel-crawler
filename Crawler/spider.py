5be0a295fa62e1ef8c405f3b29c9e38e1de765f6
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-30 10:12:48 +0900

    edit: use items.Company to yield items

--- Crawler/spider.py
+++ Crawler/spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from .items import Company, table_head_to_class_field
 
 
 class CompanySpider(scrapy.Spider):
@@ -6,15 +7,16 @@ class CompanySpider(scrapy.Spider):
     base_url = 'http://www.saramin.co.kr/zf_user/recruit/company-info-view?idx=%d'
 
     def start_requests(self):
-        for i in range(1, 100):
+        for i in range(1, 10):
             yield scrapy.Request(self.base_url % i)
 
     def parse(self, response):
-        dic = {}
+        item = Company()
         for info in response.css('.table_col_type1').css('tbody').css('tr'):
-            key = info.css('th::text').extract_first().strip()
-            if key == "홈페이지":
-                dic[key] = info.css('td a.link_site::attr("href")').extract_first()
-            else:
-                dic[key] = info.css('td::text').extract_first().strip()
-        yield dic
+            key = table_head_to_class_field(info.css('th::text').extract_first().strip())
+            value = info.css('td::text').extract_first().strip()
+            if key == "website":
+                item[key] = info.css('td a.link_site::attr("href")').extract_first()
+            elif key is not None and value is not None:
+                item[key] = value
+        yield item
