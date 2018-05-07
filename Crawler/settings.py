41b3240760db50abc1452db00dbdc3d9f9e39706
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-05-07 14:41:23 +0900

    edit: move demo's setting to setting.py

--- Crawler/settings.py
+++ Crawler/settings.py
@@ -1,3 +1,8 @@
+USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+FEED_FORMAT = 'json'
+FEED_URI = './myfile.txt'
+FEED_EXPORT_ENCODING = 'utf-8'
+
 ITEM_PIPELINES = {'newscrawling.pipelines.MongoDBPipeline': 300, }
 
 MONGODB_SERVER = "localhost"
