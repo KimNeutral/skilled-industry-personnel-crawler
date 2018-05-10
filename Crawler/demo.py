1a64832a5f2f135806edea354d766ce5b859b7c2
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-05-10 16:10:56 +0900

    edit: use setting with get_project_settinng()

--- Crawler/demo.py
+++ Crawler/demo.py
@@ -1,16 +1,13 @@
 # import dmoz spider class
 import scrapy;
 from Crawler import spider
+from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
 
 # scrapy api
 from scrapy.crawler import CrawlerProcess
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    'FEED_FORMAT': 'json',
-    'FEED_URI': './myfile.txt',
-    'FEED_EXPORT_ENCODING': 'utf-8'
-})
+process = CrawlerProcess(get_project_settings())
 
 process.crawl(spider.CompanySpider, idx=2)
 process.start() # the script will block here until the crawling is finished
